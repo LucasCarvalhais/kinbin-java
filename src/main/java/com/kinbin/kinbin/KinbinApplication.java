1645de34e89cd093e2682b19959117a2bc860026
LucasCarvalhais <lucascarvalhais03@outlook.com>
2019-08-19 12:48:46 -0300

    initial commit

---
+++ src/main/java/com/kinbin/kinbin/KinbinApplication.java
@@ -0,0 +1,13 @@
+package com.kinbin.kinbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinbinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinbin/kinbin/KinbinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kinbin.kinbin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KinbinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
