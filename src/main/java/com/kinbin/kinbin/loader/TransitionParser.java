9ee41d67bdfa23c1f27e88ffd701d0d2118a48b1
LucasCarvalhais <lucascarvalhais03@outlook.com>
2019-08-22 14:10:07 -0300

    Card Parser done

--- src/main/java/com/kinbin/kinbin/loader/TransitionParser.java
+++ src/main/java/com/kinbin/kinbin/loader/TransitionParser.java
@@ -1,5 +1,8 @@
 package com.kinbin.kinbin.loader;
 
+import com.kinbin.kinbin.core.model.Card;
+import com.kinbin.kinbin.core.model.CardType;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -14,5 +17,27 @@ public class TransitionParser {
 
     public List<String> getColumnsHeaders() {
         return columnHeaders;
+    }
+
+    public Card extractCard(String line) {
+        String[] informations = line.split(",");
+        String id = informations[0];
+        CardType cardType = parseCardType(informations[informations.length-1]);
+        return new Card(id, cardType);
+    }
+
+    private CardType parseCardType(String information) {
+        switch (information) {
+            case "Story":
+                return CardType.STORY;
+            case "Bug":
+                return CardType.DEFECT;
+            case "Spike":
+                return CardType.SPIKE;
+            case "Task":
+                return CardType.TECH_DEBT;
+            default:
+                return null;
+        }
     }
 }
--- src/test/java/com/kinbin/kinbin/loader/TransitionParserTest.java
+++ src/test/java/com/kinbin/kinbin/loader/TransitionParserTest.java
@@ -1,5 +1,7 @@
 package com.kinbin.kinbin.loader;
 
+import com.kinbin.kinbin.core.model.Card;
+import com.kinbin.kinbin.core.model.CardType;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -19,6 +21,47 @@ public class TransitionParserTest {
         assertThat(transitionParser.getColumnsHeaders().size(), is(11));
     }
 
-    
+    @Test
+    public void shouldExtractStoryCard() {
+        String line = "BKG-1,https://its.it.lan.com/browse/BKG-1,,2015-11-12,,2015-11-20,2015-12-01,,2015-12-23," +
+                "2016-01-18,Story,";
+        TransitionParser transitionParser = new TransitionParser();
 
+        Card card = transitionParser.extractCard(line);
+        assertThat(card.getId(), is("BKG-1"));
+        assertThat(card.getCardType(), is(CardType.STORY));
+    }
+
+    @Test
+    public void shouldExtractDefectCard() {
+        String line = "BKG-1,https://its.it.lan.com/browse/BKG-1,,2015-11-12,,2015-11-20,2015-12-01,,2015-12-23," +
+                "2016-01-18,Bug,";
+        TransitionParser transitionParser = new TransitionParser();
+
+        Card card = transitionParser.extractCard(line);
+        assertThat(card.getId(), is("BKG-1"));
+        assertThat(card.getCardType(), is(CardType.DEFECT));
+    }
+
+    @Test
+    public void shouldExtractSpikeCard() {
+        String line = "BKG-1,https://its.it.lan.com/browse/BKG-1,,2015-11-12,,2015-11-20,2015-12-01,,2015-12-23," +
+                "2016-01-18,Spike,";
+        TransitionParser transitionParser = new TransitionParser();
+
+        Card card = transitionParser.extractCard(line);
+        assertThat(card.getId(), is("BKG-1"));
+        assertThat(card.getCardType(), is(CardType.SPIKE));
+    }
+
+    @Test
+    public void shouldExtractTechDebtCard() {
+        String line = "BKG-1,https://its.it.lan.com/browse/BKG-1,,2015-11-12,,2015-11-20,2015-12-01,,2015-12-23," +
+                "2016-01-18,Task,";
+        TransitionParser transitionParser = new TransitionParser();
+
+        Card card = transitionParser.extractCard(line);
+        assertThat(card.getId(), is("BKG-1"));
+        assertThat(card.getCardType(), is(CardType.TECH_DEBT));
+    }
 }
