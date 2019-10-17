fef4fe6346153c22c5716aa1f0787fc35bb6554c
LucasCarvalhais <lucascarvalhais03@outlook.com>
2019-10-17 17:05:00 -0300

    Little changes in KinbinApplication.java

--- src/main/java/com/kinbin/kinbin/KinbinApplication.java
+++ src/main/java/com/kinbin/kinbin/KinbinApplication.java
@@ -1,10 +1,7 @@
 package com.kinbin.kinbin;
 
-import com.kinbin.kinbin.configuration.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class KinbinApplication {
